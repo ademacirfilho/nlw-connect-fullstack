f976688b0148f6bdc0ee1d08d8c1f3c4e9e16ce2
Ademacir Filho <ademacirfilho@gmail.com>
2025-02-21 20:00:48 -0300

    Aula 2 - NLW Connect

--- script.js
+++ script.js
@@ -1,9 +1,83 @@
-const app = document.getElementById("app")
+const app = document.getElementById('app')
+
+// array, vetor, lista
+const users = [
+    {
+        email: 'test@test.com',
+        phone: '99999999999',
+        ref: 100,
+        refBy: null
+    },
+    {
+        email: 'tust@tust.com',
+        phone: '99999999999',
+        ref: 200,
+        refBy: 100
+    },
+    {
+        email: 'tost@tost.com',
+        phone: '99999999999',
+        ref: 300,
+        refBy: 200
+    }
+]
+
+const getUser = (userData) => {
+    return users.find((user) => {
+        return user.email == userData.email
+    })
+}
+
+const getTotalSubscribers = (userData) => {
+    const subs = users.filter((user) => {
+        return user.refBy == userData.ref
+    })
+    return subs.length
+}
+
+const showInvite = (userData) => {
+    app.innerHTML = `
+    <input type="text" id="link" value="https://evento.com?ref=${userData.ref}" disabled>
+
+    <div id="stats">
+        <h4>
+            ${getTotalSubscribers(userData)}
+        </h4>
+        <p>
+            Inscrições feitas
+        </p>
+    </div>
+    `
+}
+
+const saveUser = (userData) => {
+    const newUser = {
+        ...userData,
+        ref: Math.round(Math.random() * 4000),
+        refBy: 100
+    }
+
+    users.push(newUser) // adiciona um novo item no array
+    return newUser // retorna o novo item
+}
 
 const formAction = () => {
-    const form = document.querySelector("form")
+    const form = document.querySelector('form')
     form.onsubmit = (event) => {
         event.preventDefault()
+        const formData = new FormData(form)
+        const userData = {
+            email: formData.get('email'),
+            phone: formData.get('phone'),
+        }
+
+        const user = getUser(userData)
+        if (user) {
+            showInvite(user)
+        } else {
+            const newUser = saveUser(userData)
+            showInvite(newUser)
+        }
     }
 }
 
@@ -23,3 +97,5 @@ const startApp = () => {
     formAction()
 }
 startApp()
+
+document.getElementById("logo").onclick = () => startApp()
